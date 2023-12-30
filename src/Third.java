16a2b35ced1d8a1a9720ede9020cafe8df4f5cb9
Nikita Kirikov <n.kiirkov@gmail.com>
2023-12-30 17:06:30 +0300

    add third homework

---
+++ src/Third.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class Third {
+    public static void Run() {
+        int[] array1 = {1,6,4,2,8};
+        int[] array2 = {1,8,4,6,3};
+
+        float sum1 = 0F;
+        float sum2 = 0F;
+
+        for(int i = 0; i < array1.length; i++) {
+            sum1 += array1[i];
+            sum2 += array2[i];
+        }
+
+        System.out.println(Arrays.toString(array1));
+        System.out.println(Arrays.toString(array2));
+
+        if ((sum1 / array1.length) > (sum2 / array2.length)) {
+            System.out.println("Среднее арифметическое больше у первого массива");
+        } else if ((sum2 / array2.length) > (sum1 / array1.length)) {
+            System.out.println("Среднее арифметическое больше у второго массива");
+        } else {
+            System.out.println("Среднее арифметическое равно");
+        }
+    }
+}
